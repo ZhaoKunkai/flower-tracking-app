76bd428d76e44bad3a899d39dbf81b535d5cceb2
ZhaoKunkai <2625595090@qq.com>
2024-04-28 11:11:25 +0800

    fix type typo

--- client/src/context/type.js
+++ client/src/context/type.js
@@ -2,6 +2,6 @@ export const FLOWER_ERROR = 'FLOWER_ERROR';
 export const GET_FLOWERS = 'GET_FLOWERS';
 export const ADD_FLOWERS = 'ADD_FLOWERS';
 export const DELETE_FLOWER = 'DELETE_FLOWERS';
-export const CLEAR_CURRENT = 'CLEAR_FLOWERS';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 
 
